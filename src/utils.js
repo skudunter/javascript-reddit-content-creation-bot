758dffa0e3b738269c8cf60d1d9fb9951056598f
Daniel <danielgruttercpt@gmail.com>
2023-01-12 12:37:11 +0200

    whoo

--- src/utils.js
+++ src/utils.js
@@ -3,13 +3,18 @@ export default {
     return input.replace(/[^a-z0-9 .$!?]/gi, "");
   },
   getRandomPost(posts) {
-    let max = Math.round(posts.data.children.length * 0.5);
-    let index = Math.round(getRandomValue(0, max));
-    return {
-      id: posts.data.children[index].data.id,
-      title: posts.data.children[index].data.title,
-      description: posts.data.children[index].data.selftext,
-    };
+    if (posts.data.children.length) {
+      let max = Math.round(posts.data.children.length * 0.5);
+      let index = Math.round(getRandomValue(0, max));
+      return {
+        id: posts.data.children[index].data.id,
+        title: posts.data.children[index].data.title,
+        description: posts.data.children[index].data.selftext,
+      };
+    }else{
+      console.log('Please Choose a valid subreddit');
+      process.exit(0);
+    }
   },
 };
 function getRandomValue(min, max) {
